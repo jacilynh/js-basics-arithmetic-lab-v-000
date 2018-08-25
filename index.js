f9633e09c8573c4f7242fd7ef6439f2041c0ec0e
Jacilyn Hayden <jacihayden@gmail.com>
2018-08-25 22:29:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,3 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 
 var newID = oldID + 1000000000;
-ageIsValid = 
-randomNumber = 
-randomInteger = 
-randomUserID = 
