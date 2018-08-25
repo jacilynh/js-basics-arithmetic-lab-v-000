e4aead1f31c4467c05b70f3de15147fd33044914
Jacilyn Hayden <jacihayden@gmail.com>
2018-08-25 22:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 
 var newID = oldID + 1000000000;
-var ageIsValid = Number.isNaN(currentAge)
+var ageIsValid = Number.isInteger(currentAge);
 // randomNumber = 
 // randomInteger = 
 // randomUserID = 
